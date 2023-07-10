083ccde0edb934ce937e52a1d38dcb9d0c58d44a
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2023-07-10 23:00:37 +0800

    style(style_guide): colour pallette

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -40,10 +40,10 @@ export default function Home() {
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
+            <h1 className="dark:text-white mb-4 max-w-2xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
               Next.js Enterprise Boilerplate
             </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+            <p className="text-gray-500 dark:text-gray-400 mb-6 max-w-2xl font-light md:text-lg lg:mb-8 lg:text-xl">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
               Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
               enjoyable development process.
@@ -65,10 +65,10 @@ export default function Home() {
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
             {LP_GRID_ITEMS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
+                <div className="text-blue-700 dark:bg-primary-900 mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 lg:h-12 lg:w-12">
                   {singleItem.icon}
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
+                <h3 className="dark:text-white mb-2 text-xl font-bold">{singleItem.title}</h3>
                 <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
               </div>
             ))}
