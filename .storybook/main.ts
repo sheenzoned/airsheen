a3fe2c04d8db61b7ab492439920b3713f192a909
Jonas Rafael Sarcilla <32216903+jonassarcilla@users.noreply.github.com>
2023-07-19 22:03:34 +0800

    Update contributor file (#3)
    * Update README.md
    
    * chore(font): fix window breakpoints
    
    * chore(storybook): fix window breakpoints
    
    * chore(storybook): fix window breakpoints
    
    * feat(button): create button component
    
    * chore(assets): add image thumb
    
    * Update README.md
    
    * docs(contributor): update all-contributorsrc file
    
    * docs(contributor): update all-contributorsrc file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -25,11 +25,5 @@ const config: StorybookConfig = {
     },
   },
   staticDirs: ["../public"],
-  // previewHead: (head) => `
-  //   ${head}
-  //   <style>
-  //     @import url('https://fonts.googleapis.com/css2?family=Inter+Tight:wght@100&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-  //   </style>
-  // `,
 }
 export default config
